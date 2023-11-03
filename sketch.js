de59d730f73542910ec959490498adc330cb9b20
Pollodiaz9090 <125395930+Pollodiaz9090@users.noreply.github.com>
2023-11-02 19:17:39 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let gotData, consoleArray;
+
+function setup() {
+  createCanvas(1872, 958);
+}
+
+function preload() {
+  loadJSON("consoleSales.json", getData);
+}
+function getData(data) {
+  gotData = data;
+}
+
+function draw() {
+  background('gold');
+
+  if (gotData) {
+    let numRows = Object.keys(gotData).length;
+    consoleArray = Object.keys(gotData).map((key) => [key, gotData[key]]);
+
+    for (let i = 0; i < numRows; i++) {
+      let x = 50;
+      let y = 100 + (i * 100);
+      let w = consoleArray[i][1].globalSales*0.0000020;
+      let h = 35;
+      push();
+      let colorPick1 = consoleArray[i][1].color[0];
+      // fill(0, 0, 255);
+      let colorPick2 = consoleArray[i][1].color[1];
+      // fill(0, 0, 255);
+      let colorPick3 = consoleArray[i][1].color[2];
+      fill(colorPick1, colorPick2, colorPick3);
+      rect(x, y, w, h);
+      textSize(25);
+      text(consoleArray[i][1].name, x, y - 5);
+      pop();
+    }
+  }
+}
